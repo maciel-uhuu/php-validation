ade9a4d8e62e471262c7a281c6ff425acee97a1f
Samuel Porto <samuelalcala2001@outlook.com>
2023-06-10 19:37:21 -0300

    test(users): store an user

--- backend/tests/Feature/UserTest.php
+++ backend/tests/Feature/UserTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class UserTest extends TestCase
@@ -11,10 +12,30 @@ class UserTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    // public function test_example(): void
+    // {
+    //     $response = $this->get('/');
 
-        $response->assertStatus(200);
+    //     $response->assertStatus(200);
+    // }
+
+    public function test_store_an_client(): void
+    {
+        $ramdomEmail = rand(1, 1000) . '@gmail.com';
+        $ramdomDocument = rand(10000000000, 99999999999);
+
+
+        $reponse = $this->post('/api/users', [
+            'name' => 'Teste',
+            'email' => $ramdomEmail,
+            'password' => '123456',
+            'type' => 0,
+            'document' => $ramdomDocument,
+            'phone' => '12345678910',
+            'address' => 'Rua teste',
+            'status' => 1,
+        ]);
+
+        $reponse->assertStatus(201);
     }
 }
